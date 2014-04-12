664a07d0e35b65800001d85b88236e8531a7cd1a
itkoren <itkoren@gmail.com>
2014-04-12 21:47:32 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,13 +21,25 @@ window.onload = function() {
   var query = window.location.search.substring(1);
   var params = parseQueryString(query);
   var code = document.getElementById("gist");
+  var processed = 0;
   var attr;
   
   if (code && params) {
       for (attr in params) {
           if (params.hasOwnProperty(attr) && params[attr]) {
               code.setAttribute(attr, params[attr]);
+              processed++;
           }
       }
+  }
+  
+  if (0 < processed) {
+    var po = document.createElement("script"); 
+    po.setAttribute("type", "text/javascript"); 
+    po.setAttribute("async", true);
+    po.setAttribute("src", "//cdnjs.cloudflare.com/ajax/libs/gist-embed/1.7/gist-embed.min.js");
+    
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(po, s);
   }
 };
