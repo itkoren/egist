c3a9eb20e3bb635b7752dc270148bc7d136891e5
Itai Koren <itkoren@gmail.com>
2015-05-28 16:55:28 +0300

    fix onload

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,45 +1,45 @@
 var parseQueryString = function(queryString) {
     var params = {}
     var queries;
-    var temp; 
+    var temp;
     var i;
     var l;
- 
+
     // Split into key/value pairs
     queries = queryString.split("&");
- 
+
     // Convert the array of strings into an object
     for (i = 0, l = queries.length; i < l; i++) {
         temp = queries[i].split("=");
         params[temp[0]] = decodeURIComponent(temp[1]);
     }
- 
+
     return params;
 };
 
-window.onload = function() {
-  var query = window.location.search.substring(1);
-  var params = parseQueryString(query);
-  var code = document.getElementById("gist");
-  var processed = 0;
-  var attr;
-  
-  if (code && params) {
-      for (attr in params) {
-          if (params.hasOwnProperty(attr) && params[attr]) {
-              code.setAttribute(attr, params[attr]);
-              processed++;
-          }
-      }
-  }
-  
-  if (0 < processed) {
-    var po = document.createElement("script"); 
-    po.setAttribute("type", "text/javascript"); 
-    po.setAttribute("async", true);
-    po.setAttribute("src", "//cdnjs.cloudflare.com/ajax/libs/gist-embed/2.1/gist-embed.min.js");
-    
-    var s = document.getElementsByTagName("script")[0]; 
-    s.parentNode.insertBefore(po, s);
-  }
-};
+document.addEventListener("DOMContentLoaded", function(event) {
+    var query = window.location.search.substring(1);
+    var params = parseQueryString(query);
+    var code = document.getElementById("gist");
+    var processed = 0;
+    var attr;
+
+    if (code && params) {
+        for (attr in params) {
+            if (params.hasOwnProperty(attr) && params[attr]) {
+                code.setAttribute(attr, params[attr]);
+                processed++;
+            }
+        }
+    }
+
+    if (0 < processed) {
+        var po = document.createElement("script");
+        po.setAttribute("type", "text/javascript");
+        po.setAttribute("async", true);
+        po.setAttribute("src", "//cdnjs.cloudflare.com/ajax/libs/gist-embed/2.1/gist-embed.min.js");
+
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(po, s);
+    }
+});
